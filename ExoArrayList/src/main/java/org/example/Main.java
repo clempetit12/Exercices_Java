457a2a076ce54136b893dfd0de5658059eddddd9
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-11-28 11:37:19 +0100

    exo bibliothèque

---
+++ ExoArrayList/src/main/java/org/example/Main.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import org.example.classes.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Book> booklist = new ArrayList<Book>();
+        ArrayList<Emprunt> empruntList = new ArrayList<Emprunt>();
+
+        LocalDate dateChoisie = LocalDate.of(2023,11,28);
+
+
+        Library library = new Library(booklist, empruntList);
+
+
+        PaperBook book = new PaperBook("Danser", "Steinbeck", 500, "Hachette");
+        PaperBook book2 = new PaperBook("Danser", "Steinbeck", 500, "Hachette");
+        NumericBook book3 = new NumericBook("Rêver", "Frank Thilliez", Format.EPUB, 300);
+        NumericBook book4 = new NumericBook("Le bleu du ciel", "Melissa Costa", Format.PDF, 400);
+
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+
+
+        //Affichage livres bibliothèques et taille bibliothèque
+        System.out.println("Les livres dans la bibliothèque ");
+        for (Book b : booklist) {
+            System.out.println(b);
+        }
+        System.out.println();
+        System.out.println("La taille de la bibliothèque avant retrait livre : " + booklist.size());
+
+        //Suppression book2
+        library.removeBook(book2);
+        System.out.println("La taille de la bibliothèque après retrait livre est : " + booklist.size());
+        System.out.println();
+
+        //Affichage livres bibliothèques après retrait
+        System.out.println("Les livres dans la bibliothèque ");
+        for (Book b : booklist) {
+            System.out.println(b);
+        }
+
+        System.out.println();
+//Trouver livre par titre
+        System.out.println("Le livre par titre : ");
+        library.findBookbyTitle("Danser");
+        System.out.println();
+
+        //Trouver livre par auteur
+        System.out.println("Lelivre par auteur : ");
+        library.findBookbyAuthor("Sophocle");
+        System.out.println();
+
+        //Emprunter un livre
+
+        Person person = new Person("Clara", "Taram");
+        Person person2 = new Person("Athéna", "Sapi");
+
+
+        library.empruntBook(book, person);
+        library.empruntBook(book2, person2);
+
+        System.out.println("Liste emprunt  : ");
+        for (Emprunt e : empruntList) {
+
+            System.out.println(e);
+        }
+
+        System.out.println();
+        // Retourner le livre
+        LocalDate dateFictive = LocalDate.of(2023,10,12);
+
+        System.out.println("Liste emprunt après avoir rendu le livre : ");
+        library.returnBook(book2 ,dateFictive);
+        for (Emprunt e : empruntList) {
+            System.out.println(e);
+        }
+
+
+    }
+
+
+}
---
+++ ExoArrayList/src/main/java/org/example/classes/Library.java
@@ -0,0 +1,77 @@
+package org.example.classes;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Library {
+
+    public ArrayList<Book> bookList;
+    public ArrayList<Emprunt> empruntList;
+
+    public Library(ArrayList<Book> bookList, ArrayList<Emprunt> empruntList) {
+        this.bookList = bookList;
+        this.empruntList = empruntList;
+    }
+
+    public void addBook(Book book) {
+        bookList.add(book);
+    }
+
+
+
+    public void findBookbyTitle(String title) {
+        for (Book b : bookList) {
+            if (title.equals(b.getTitle())) {
+                System.out.println(b);
+            }
+        }
+    }
+
+    public void findBookbyAuthor(String author) {
+        for (Book b : bookList) {
+            if (author.equals(b.getAuthor())) {
+                System.out.println(b);
+            }
+        }
+    }
+    public void removeBook(Book book) {
+        int idBook = book.getId();
+        Book foundBook = null;
+        for (Book b:bookList) {
+            if (idBook == b.getId()) {
+                foundBook = b;
+                break;
+            }
+        }
+        if ((foundBook != null)) {
+            bookList.remove(foundBook);
+        }}
+
+    public void empruntBook(Book book, Person person) {
+        int idBook = book.getId();
+        LocalDate dateChoisie = LocalDate.of(2023,05,28);
+        Emprunt newEmprunt = new Emprunt(dateChoisie, dateChoisie.plusMonths(1), person, book);
+        empruntList.add(newEmprunt);
+
+
+    }
+
+    public void returnBook(Book book, LocalDate date) {
+        Emprunt foundEmprunt = null;
+        int bookId = book.getId();
+        for (Emprunt e : empruntList) {
+            if (bookId == e.getBook().getId()) {
+                foundEmprunt = e;
+            }
+        }
+          if (date.isAfter(foundEmprunt.getEndingDate())) {
+              System.out.println("Vous avez dépassé la date vous serez facturé ");
+            } else {
+                empruntList.remove(foundEmprunt);
+            }
+        }
+
+
+    }
+
+
