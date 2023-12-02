eb509d3b5550a0621e06c3b8f2431e52d3de6096
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-12-02 15:06:10 +0100

    exo billeterie fini

--- TP_Billeterie/src/main/java/org/example/classes/Customer.java
+++ TP_Billeterie/src/main/java/org/example/classes/Customer.java
@@ -7,74 +7,95 @@ import java.util.HashMap;
 
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+
 public class Customer {
-    @NonNull
+
     private String firstname;
-    @NonNull
+
     private String id;
-    @NonNull
+
     private String lastname;
-    @NonNull
+
     private String email;
-    private ArrayList<Event> listTicketsBought;
+    private ArrayList<String> listTicketsBought = new ArrayList<>();
     private static HashMap<String, Customer> customerList = new HashMap<>();
 
-    public Customer(@NonNull String firstname, @NonNull String id, @NonNull String lastname, @NonNull String email, ArrayList<Event> listTicketsBought) {
+    public Customer(String id, String firstname, String lastname, String email) {
         this.firstname = firstname;
         this.id = id;
         this.lastname = lastname;
         this.email = email;
         this.listTicketsBought = listTicketsBought;
-        customerList.put(id,this);
+        customerList.put(id, this);
     }
 
-    public void buyTicket(Event event) {
-        if (event.checkDisponibilityTicket()) {
-            listTicketsBought.add(event);
-            event.sellTicket();
+    public static Customer getCustomer(String name) {
+        return customerList.get(name);
+    }
 
+    public void buyTicket(String eventName) {
+        if (listTicketsBought == null) {
+            ArrayList<String> listTicketsBought = new ArrayList<>();
+            listTicketsBought.add(eventName);
+            System.out.println(listTicketsBought);
         } else {
-            System.out.println("Il n'y a plus de disponibilités pour cet évènement !");
+            listTicketsBought.add(eventName);
+            System.out.println(listTicketsBought);
         }
 
-    }
-    public void cancelCustomerTicket(Event event) {
-        event.cancelTicket();
-            listTicketsBought.remove(event);
-            System.out.println("Votre billet a bien été annulé !");
-
 
     }
 
-    public void displayEvents () {
-        if (listTicketsBought == null){
-            ArrayList<Event> listTicketsBought = new ArrayList<>();
+    public boolean cancelCustomerTicket(String eventName) {
+        boolean eventFound = false;
+        for (String e : listTicketsBought) {
+            if (eventName.contains(e)) {
+                listTicketsBought.remove(e);
+                System.out.println("Votre billet a bien été annulé !");
+                eventFound = true;
+                break;
+            }
+        }
 
+            if (!eventFound) {
+                System.out.println("Je ne trouve pas cet évènement dans vos billets !");
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+    public void displayListTicketsBought(String id) {
+        if (listTicketsBought.isEmpty()) {
+            System.out.println("Il n'y a pas encore de tickets achetés)");
         } else {
-            System.out.println("Vos prochains évènements : ");
-            for (Event e: listTicketsBought) {
-                System.out.println(e);
+            for (String s : listTicketsBought) {
+                System.out.println("Les tickets achetés par " + id + " sont les suivants : " + s);
             }
         }
     }
+
     public static void modifyCustomer(String id, String lastname, String firstname, String email) {
         Customer customerUpdate = customerList.get(id);
 
         if (customerUpdate != null) {
-         customerUpdate.setFirstname(firstname);
-         customerUpdate.setLastname(lastname);
-         customerUpdate.setEmail(email);
+            customerUpdate.setFirstname(firstname);
+            customerUpdate.setLastname(lastname);
+            customerUpdate.setEmail(email);
             System.out.println("Client modifié avec succès !");
         } else {
             System.out.println("Client non trouvé. La modification a échoué.");
         }
     }
+
     public static void displayAllCustomers() {
 
         for (Customer customer : customerList.values()) {
             System.out.println(customer);
-        }}
+        }
+    }
 
     public static void deleteClient(String id) {
         Customer customerToDelete = customerList.get(id);
--- TP_Billeterie/src/main/java/org/example/classes/Event.java
+++ TP_Billeterie/src/main/java/org/example/classes/Event.java
@@ -26,81 +26,108 @@ public class Event {
         this.nameEvent = nameEvent;
         this.date = date;
         this.hour = hour;
-        this.location = Location.getLocation(locationName);
-        this.price = price;
-        this.numberOfTicketsSold = numberOfTicketsSold;
-        bdd.put(nameEvent, this);
+        Location location = Location.getLocation(locationName);
+        if (location == null) {
+            System.out.println("Erreur : Le lieu '" + locationName + "' n'existe pas.");
+        } else {
+            this.location = location;
+            this.price = price;
+            this.numberOfTicketsSold = numberOfTicketsSold;
+            bdd.put(nameEvent, this);
+        }
     }
 
-    public static boolean checkDisponibilityTicket(String eventName) {
-        Event eventWished = bdd.get(eventName);
-        if (eventWished != null) {
-            long capacity = eventWished.location.getCapacity();
-            long realCapacity = capacity - eventWished.numberOfTicketsSold;
-            System.out.println("La capacité réelle est de :" + realCapacity);
-            return eventWished.numberOfTicketsSold < realCapacity;
-        } else {
-            System.out.println("Il n'y a pas d'évènements ");
-            return false;
+        public String getNameEvent () {
+            return nameEvent;
+        }
+        public static Event getEvent (String name){
+            return bdd.get(name);
         }
 
-    };
 
-    public void sellTicket(String eventName) {
-        if (checkDisponibilityTicket(eventName)) {
-            numberOfTicketsSold++;
-            System.out.println("Le nombre de tickets vendus s'élèvent à " + numberOfTicketsSold);
+        public static void setBdd (HashMap < String, Event > bdd){
+            Event.bdd = bdd;
+        }
+
+        public void setNameEvent (String nameEvent){
+            this.nameEvent = nameEvent;
+        }
+
+        public static boolean checkDisponibilityTicket (String eventName){
+            Event eventWished = bdd.get(eventName);
+            if (eventWished != null) {
+                long capacity = eventWished.location.getCapacity();
+                long realCapacity = capacity - eventWished.numberOfTicketsSold;
+                System.out.println("La capacité réelle est de :" + realCapacity);
+                return eventWished.numberOfTicketsSold < realCapacity;
+            } else {
+                System.out.println("Il n'y a pas d'évènements ");
+                return false;
+            }
+
         }
         ;
-    };
 
-    public void cancelTicket() {
-        numberOfTicketsSold--;
-        System.out.println("Le nombre de tickets vendus s'élèvent à " + numberOfTicketsSold);
-    }
-    public static void modifyEvent(String nameEvent, LocalDate date, LocalTime hour, String locationName, int price, int numberOfTicketsSold) {
-        Event eventUpdate = bdd.get(nameEvent);
+        public void sellTicket (String eventName){
+            if (checkDisponibilityTicket(eventName)) {
+                numberOfTicketsSold++;
+                System.out.println("Le nombre de tickets vendus s'élèvent à " + numberOfTicketsSold);
+            }
+            ;
+        }
+        ;
 
-        if (eventUpdate != null) {
-            eventUpdate.setDate(date);
-            eventUpdate.setHour(hour);
-            eventUpdate.setLocationName(locationName);
-            eventUpdate.setPrice(price);
-            eventUpdate.setNumberOfTicketsSold(numberOfTicketsSold);
-            System.out.println("Evenement modifié avec succès !");
-        } else {
-            System.out.println("Evenement non trouvé. La modification a échoué.");
+        public void cancelTicket () {
+            numberOfTicketsSold--;
+            System.out.println("Le nombre de tickets vendus s'élèvent à " + numberOfTicketsSold);
+        }
+        public static void modifyEvent (String nameEvent, LocalDate date, LocalTime hour, String locationName,int price,
+        int numberOfTicketsSold){
+            Event eventUpdate = bdd.get(nameEvent);
+
+            if (eventUpdate != null) {
+                eventUpdate.setDate(date);
+                eventUpdate.setHour(hour);
+                eventUpdate.setLocationName(locationName);
+                eventUpdate.setPrice(price);
+                eventUpdate.setNumberOfTicketsSold(numberOfTicketsSold);
+                System.out.println("Evenement modifié avec succès !");
+            } else {
+                System.out.println("Evenement non trouvé. La modification a échoué.");
+            }
+        }
+        public static void displayAllEvents () {
+
+            for (Event event : bdd.values()) {
+                System.out.println(event);
+            }
+        }
+        public static void deleteEvent (String name){
+            Event eventToDelete = bdd.get(name);
+
+            if (eventToDelete != null) {
+                bdd.remove(name);
+                System.out.println("L'évènement a été supprimé avec succès !");
+
+            } else {
+                System.out.println("Evenement non trouvé. La suppression a échoué.");
+            }
+        }
+
+        @Override
+        public String toString () {
+            return "Event{" +
+                    "nameEvent='" + nameEvent + '\'' +
+                    ", date=" + date +
+                    ", hour=" + hour +
+                    ", location=" + location +
+                    ", price=" + price +
+                    ", numberOfTicketsSold=" + numberOfTicketsSold +
+                    '}';
         }
     }
-    public static void displayAllEvents() {
 
-        for (Event event : bdd.values()) {
-            System.out.println(event);
-        }}
-    public static void deleteEvent(String name) {
-        Event eventToDelete = bdd.get(name);
-
-        if (eventToDelete != null) {
-            bdd.remove(name);
-            System.out.println("L'évènement a été supprimé avec succès !");
-
-        } else {
-            System.out.println("Evenement non trouvé. La suppression a échoué.");
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "nameEvent='" + nameEvent + '\'' +
-                ", date=" + date +
-                ", hour=" + hour +
-                ", location=" + location +
-                ", price=" + price +
-                ", numberOfTicketsSold=" + numberOfTicketsSold +
-                '}';
-    }
-};
+    ;
 
 
 
