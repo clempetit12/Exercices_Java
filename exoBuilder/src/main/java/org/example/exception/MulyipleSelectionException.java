99dc0d9a5b3b8fcf82f711a7dc57a44fba312189
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-12-05 10:57:12 +0100

    exo pizza

--- exoBuilder/src/main/java/org/example/exception/MulyipleSelectionException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class MulyipleSelectionException extends Exception {
-    public MulyipleSelectionException(String message) {
-        super(message);
-    }
-}
