24a5b29283600d8854ff4d21a2aebe923d0ca34c
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2024-01-09 11:25:43 +0100

    c

--- JPA/Exercice1/tdoList.sql
+++ JPA/Exercice1/tdoList.sql
@@ -4,4 +4,6 @@ use todoList;
 SELECT * FROM todoList.Task;
 SELECT * FROM todoList.tâcheinfo;
 SELECT * FROM todoList.utilisateur;
+SELECT * FROM todoList.Categorie;
+SELECT * FROM todoList.Category_task;
 DROP DATABASE todoList;
--- JPA/TP_JPA/bank.sql
+++ JPA/TP_JPA/bank.sql
@@ -3,3 +3,6 @@ use bank_bdd;
 SELECT * FROM bank_bdd.Agency;
 SELECT * FROM bank_bdd.Customers;
 SELECT * FROM bank_bdd.Customers_accounts;
+SELECT * FROM bank_bdd.Accounts;
+
+drop database bank_bdd;
