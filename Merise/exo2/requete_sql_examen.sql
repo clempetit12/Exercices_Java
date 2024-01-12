7c1984d671290ec4aed84e9388fdee2fb83c322b
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2024-01-12 19:48:48 +0100

    c

--- Merise/exo2/requete_sql_examen.sql
+++ Merise/exo2/requete_sql_examen.sql
@@ -10,7 +10,8 @@ ville_etablissement VARCHAR(50)
 
 CREATE TABLE IF NOT EXISTS examens (
 id_ex INT AUTO_INCREMENT PRIMARY KEY,
-theme_examen VARCHAR(200)
+theme_examen VARCHAR(200),
+date_examen DATE
 );
 
 CREATE TABLE IF NOT EXISTS epreuves (
@@ -38,7 +39,9 @@ id_el INT AUTO_INCREMENT PRIMARY KEY,
 nom_eleve VARCHAR(200),
 date_naissance_eleve DATE,
 id_e INT,
-CONSTRAINT fk_etablissement_eleves_id FOREIGN KEY (id_e) REFERENCES etablissement(id_e)
+id_ex INT,
+CONSTRAINT fk_etablissement_eleves_id FOREIGN KEY (id_e) REFERENCES etablissement(id_e),
+CONSTRAINT fk_examen_eleves_id FOREIGN KEY (id_ex) REFERENCES examens(id_ex)
 );
 
 CREATE TABLE IF NOT EXISTS dossier_inscription (
@@ -93,35 +96,48 @@ INSERT INTO  eleves (nom_eleve,date_naissance_eleve,id_e,id_ex)
 VALUES
  ("thomas","2001-01-01",3,1),
  ("jean","2001-01-01",1,1),
- ("lea,","2001-01-01",2,2);
- 
- INSERT INTO  examens (theme_examen) 
+ ("lea,","2001-01-01",2,2),
+ ("eloise,","2001-01-01",3,1);
+ INSERT INTO  eleves (nom_eleve,date_naissance_eleve,id_e) 
 VALUES
- ("baccalaureat"),
- ("brevet");
+ ("thomas","2001-01-01",3),
+ ("jean","2001-01-01",1);
+
+ 
+ INSERT INTO  examens (theme_examen,date_examen) 
+VALUES
+ ("baccalaureat","2023-06-12"),
+ ("brevet","2023-05-12");
  
   INSERT INTO  dossier_inscription (id_el) 
 VALUES
  (1),
  (2),
- (3);
+ (3),
+ (4),
+ (4);
    INSERT INTO  examen_eleve (id_el,id_ex) 
 VALUES
  (1,1),
  (2,1),
  (3,2);
  
-INSERT INTO  epreuves (coefficient_epreuve,id_ex,nom_epreuve) 
+INSERT INTO  epreuves (coefficient_epreuve,id_ex,nom_epreuve,date) 
 VALUES
- (2,1, "math"),
- (1,1, "art"),
- (2,2, "histoire"),
- (2,1, "français");
+ (2,1, "sciences","2023-06-12"),
+ (1,1, "plastique", "2023-06-15"),
+ (2,2, "histoire", "2023-07-18"),
+ (2,1, "français", "2023-05-11");
  
 INSERT INTO  resultat (id_el,id_ep,note) 
 VALUES
  (1,1,5),
- (2,1,15);
+ (1,2,14),
+ (1,4,16),
+  (2,1,9),
+ (2,2,11),
+ (2,4,18),
+ (3,3,15);
  
  INSERT INTO  etablissement (nom_etablissement,adresse_etablissement,ville_etablissement) 
 VALUES
@@ -137,13 +153,13 @@ VALUES
  
   INSERT INTO  corriger (id_en,id_ex) 
 VALUES
- (4,1),
- (5,1);
+ (1,1),
+ (2,1);
  
    INSERT INTO  rediger (id_en,id_ex) 
 VALUES
- (4,1),
- (5,2);
+ (1,1),
+ (2,2);
 
 ALTER TABLE epreuves
 ADD COLUMN nom_epreuve VARCHAR(200); 
@@ -160,26 +176,55 @@ SELECT * FROM resultat;
 SELECT * FROM epreuves;
 SELECT * FROM enseignants;
 
-SELECT avg(note) FROM resultat where id_ep = 1 ;
+SELECT AVG(moyenne_individuelle) AS moyenne_globale
+FROM (
+    SELECT id_el, 
+           SUM(note * coefficient_epreuve) / SUM(coefficient_epreuve) AS moyenne_individuelle
+    FROM resultat r
+    INNER JOIN epreuves e ON r.id_ep = e.id_ep
+    INNER JOIN examens ex ON e.id_ex = ex.id_ex
+    WHERE ex.theme_examen = 'baccalaureat'
+    GROUP BY id_el
+) AS moyennes_individuelles;
+
+ 
 SELECT * FROM enseignants WHERE id_e IN (select id_e from etablissement where ville_etablissement = "paris");
 SELECT id_e, COUNT(id_el) AS nombre_eleves FROM eleves GROUP BY id_e;
 select nom_eleve from eleves where id_el in (select id_el from resultat where note > 10) ;
-SELECT *
-FROM epreuves e
-JOIN examens ex ON e.id_ex = ex.id_ex;
 
+ SELECT r.id_el, el.nom_eleve,
+           note 
+    FROM resultat r
+    INNER JOIN eleves el ON el.id_el = r.id_el
+    WHERE note > 10 group by r.id_el,note;
+    
+SELECT ex.id_ex,e.nom_epreuve,e.coefficient_epreuve
+FROM examens ex
+JOIN epreuves e ON e.id_ex = ex.id_ex;
+
+select e.nom_etablissement, COUNT(el.id_ex) as nombre_examens
+from etablissement e
+join eleves el on e.id_e = el.id_e group by e.nom_etablissement ;
 
 select * from eleves where id_ex IS NULL ;
 
-SELECT *
-FROM rediger r
+SELECT en.nom_enseignant
+from enseignants en
+join rediger r on en.id_en = r.id_en
 WHERE (r.id_en, r.id_ex) IN (
     SELECT c.id_en, c.id_ex
     FROM corriger c
     WHERE r.id_en = c.id_en AND r.id_ex = c.id_ex
-);
+) group by en.nom_enseignant;
 
+select el.nom_eleve, MAX(ep.date), r.note from
+eleves el 
+join resultat r on r.id_el = el.id_el
+join epreuves ep on el.id_ex = ep.id_ex group by el.nom_eleve,r.note;
+
+alter table epreuves add column date DATE;
 
 drop table examen_dossier;
 drop table examen_enseignant;
 drop table examen_eleve;
+drop database examens;
