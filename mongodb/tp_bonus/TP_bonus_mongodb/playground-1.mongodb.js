70c7b41e2c6f8fcaffeea3145f316eca69b6f5c5
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2024-01-22 11:51:11 +0100

    commit

---
+++ mongodb/tp_bonus/TP_bonus_mongodb/playground-1.mongodb.js
@@ -0,0 +1,189 @@
+// Quels sont les sportifs (identifiant, nom et prénom) qui ont un âge entre 20 et 30 ans ?
+
+use("Bonus");
+
+//db.sportifs.find({ Age: { $gt: 20, $lt: 31 } }, { _id: 1, Nom: 1, Prenom: 1 })
+
+// Quels sont les gymnases de ville “Villetaneuse” ou de “Sarcelles” qui ont une surface de plus de 400 m2 
+
+// db.gymnase.find(
+//     { Ville: { $regex: /^(Villetaneuse|Sarcelles)$/i }, Surface: { $gte: 400 } },
+//     { _idGymnase: 1, Ville: 1, Surface: 1 }
+//   )
+
+// Quels sont les sportifs (identifiant et nom) qui pratiquent du handball 
+
+// db.sportifs.aggregate([
+//     {
+//       $match: {
+//         "Sports.Jouer": "Hand ball"
+//       }
+//     },
+//     {
+//       $project: {
+//         _id: 1,
+//         Nom: 1
+//       }
+//     }
+//   ])
+
+  // Quels sportifs (identifiant et nom) ne pratiquent aucun sport ?
+
+//   db.sportifs.aggregate([
+//     {
+//         $match: {
+            
+//             "Sports.Jouer" : {$exists : false}
+           
+//         }
+//     },
+//     {
+//         $project : {
+//             _id : 1,
+//             Nom : 1
+//         }
+//     }
+//   ])
+  
+// Quels gymnases n’ont pas de séances le dimanche ?
+// db.gymnase.find({"Seances.Jour": {$not: {$regex: /^dimanche$/i}}})
+
+
+// Quels gymnases ne proposent que des séances de basketball ou de volley ball ?
+// db.gymnase.aggregate([
+//     {
+//       $match: {
+//         "Seances.Libelle": {
+//           $all: ["Basket ball", "Volley ball"]
+//         }
+//       }
+//     },
+//     {
+//       $project: {
+//         _id: 1,
+//         NomGymnase: 1,
+//         "Seances.Libelle" : 1
+//       }
+//     }
+//   ])
+  
+  
+  
+  
+  
+
+  // Quels sont les entraîneurs qui sont aussi joueurs 
+
+
+//     db.sportifs.aggregate([
+//     {
+//         $match: {
+//             $and:[     {"Sports.Jouer" : {$exists : true},"Sports.Entrainer" : {$exists : true}}],
+           
+           
+   
+           
+//         }
+//     },
+//     {
+//         $project : {
+//             _id : 1,
+//             Nom : 1,
+//             "Sports.Jouer" : 1,
+//             "Sports.Entrainer" : 1
+//         }
+//     }
+//   ])
+
+
+// Pour le sportif “Kervadec” quel est le nom de son conseiller ?
+
+//db.sportifs.find({Nom : "KERVADEC"})
+
+// db.sportifs.aggregate([
+//     {
+//       $match: {
+//        Nom : "KERVADEC"
+//       }
+//     },
+//     {
+//       $lookup: {
+//         from: "sportifs",
+//         localField: "IdSportifConseiller",
+//         foreignField: "IdSportif",
+//         as: "conseiller"
+//       }
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         Nom : 1,
+//         NomConseiller: "$conseiller.Nom",
+//         PrenomConseiller: "$conseiller.Prenom"
+//       }
+//     }
+//   ])
+  
+
+  // Quelle est la moyenne d’âge des sportives qui pratiquent du basket ball ?
+  
+//   db.sportifs.aggregate([
+//     {
+//       $match: {
+//         "Sports.Jouer": "Basket ball",
+//         Sexe: "F"
+//       }
+//     },
+//     {
+//       $group: {
+//         _id: 1,
+//         note_moyenne: { $avg: "$Age" }
+//       }
+//     }
+//   ])
+  
+
+  // Quels entraîneurs n’entraînent que du hand ball ou du basketball ?
+//   db.sportifs.aggregate([
+//     {
+//       $match: {
+//         $or: [
+//           {
+//             $and: [
+//               {
+//                 "Sports.Entrainer": {
+//                   $in: [/^basket ball$/i, /^hand ball$/i]
+//                 }
+//               },
+//               {
+//                 "Sports.Entrainer": {
+//                   $size: 2
+//                 }
+//               }
+//             ]
+//           },
+//           {
+//             $and: [
+//               {
+//                 "Sports.Entrainer": {
+//                   $in: [/^basket ball$/i, /^hand ball$/i]
+//                 }
+//               },
+//               {
+//                 "Sports.Entrainer": {
+//                   $size: 1
+//                 }
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     }
+//   ]) 
+  
+  // Pour chaque sportif donner le nombre de sports qu’il arbitre?
+
+  db.sportifs.aggregate({$group : {_id : "$Nom", nombre_sports_arbitrer : {$sum : 1}}})
+
+  
+  
